4a789481b43455bc051fca8575771ed9b4fe91ee
Christian Holl <cyborgx1@gmail.com>
2012-04-23 16:31:19 +0000

    removed unnecessary stuff

---
+++ aa_signs/src/signDetection.cpp
@@ -0,0 +1,154 @@
+#include <ros/ros.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <math.h>
+#include <ros/ros.h>
+#include <dynamic_reconfigure/server.h>
+//#include <htwAalen_signDetect/disturbance_filter_calibratorConfig.h>
+#include <image_geometry/pinhole_camera_model.h>
+
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_geometry/pinhole_camera_model.h>
+#include <sstream>
+#include <set>
+#include <iterator>
+#include <fstream>
+#include <iostream>
+
+#include "aa_signs/signDetectionConfig.h"
+
+
+namespace enc = sensor_msgs::image_encodings;
+
+
+class signDetection
+{
+	//NodeHandle
+	ros::NodeHandle nh_;
+
+	//Dynamic reconfigure
+	dynamic_reconfigure::Server<aa_signs::signDetectionConfig> reconfServer;
+	dynamic_reconfigure::Server<aa_signs::signDetectionConfig>::CallbackType reconfCbType;
+
+	//Types for different mats
+	typedef cv::Vec<float, 1> Vec1flt;
+	typedef cv::Vec<uchar, 3> Vec3char;
+	typedef cv::Vec<short, 1> Vec1shrt;
+	typedef cv::Vec<uchar, 1> Vec1char;
+
+	//Node handle
+	ros::NodeHandlePtr rgb_nh_;
+	boost::shared_ptr<image_transport::ImageTransport> rgb_it_, depth_it_;
+
+
+	//Image transports and synchronizers (stolen from xyzrgb-pointcloud-nodelet :-) *thanks* )
+	image_transport::SubscriberFilter sub_depth_, sub_rgb_;
+
+	message_filters::Subscriber<sensor_msgs::CameraInfo> sub_info_;
+
+	typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image,
+			sensor_msgs::Image, sensor_msgs::CameraInfo> SyncPolicy;
+
+	typedef message_filters::Synchronizer<SyncPolicy> Synchronizer;
+
+	boost::shared_ptr<Synchronizer> sync_;
+
+	image_geometry::PinholeCameraModel model_;
+
+public:
+	signDetection() :
+			nh_("~")
+	{
+		rgb_nh_.reset(new ros::NodeHandle(nh_, "rgb"));
+		ros::NodeHandle depth_nh(nh_, "depth_registered");
+		rgb_it_.reset(new image_transport::ImageTransport(*rgb_nh_));
+		depth_it_.reset(new image_transport::ImageTransport(depth_nh));
+
+		// Read parameters
+		int queue_size;
+		nh_.param("queue_size", queue_size, 5);
+
+		// Synchronize inputs. Topic subscriptions happen on demand in the connection callback.
+		sync_.reset(
+				new Synchronizer(SyncPolicy(queue_size), sub_depth_, sub_rgb_,
+						sub_info_));
+		sync_->registerCallback(
+				boost::bind(&signDetection::imageCb, this, _1, _2, _3));
+
+		//Advertise out camera
+		reconfCbType = boost::bind(&signDetection::reconfigCb, this, _1, _2);
+		reconfServer.setCallback(reconfCbType);
+
+		//Subscribe topics
+		sub_depth_.subscribe(*depth_it_, "image_rect", 1);
+		sub_rgb_.subscribe(*rgb_it_, "image_rect_color", 1);
+		sub_info_.subscribe(*rgb_nh_, "camera_info", 1);
+
+
+	}
+
+	~signDetection()
+	{
+
+	}
+
+	void reconfigCb(aa_signs::signDetectionConfig &config,
+			uint32_t level)
+	{
+
+	}
+
+	void imageCb(const sensor_msgs::ImageConstPtr& depth_msg,
+			     const sensor_msgs::ImageConstPtr& rgb_msg,
+			     const sensor_msgs::CameraInfoConstPtr& info_msg)
+	{
+
+		image_geometry::PinholeCameraModel model;
+		model.fromCameraInfo(info_msg);
+		cv_bridge::CvImagePtr imgPtrDepth, imgPtrRGB;
+
+
+
+
+
+		//Kinect raw image (millimeters)
+		if (depth_msg->encoding == "16UC1")
+		{
+			try
+			{
+				imgPtrDepth = cv_bridge::toCvCopy(depth_msg, "16UC1");
+				imgPtrRGB = cv_bridge::toCvCopy(rgb_msg, "bgr8");
+
+			} catch (cv_bridge::Exception& e)
+			{
+				ROS_ERROR("cv_bridge exception: %s", e.what());
+				return;
+			}
+
+
+	}
+
+};
+
+};
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "signDetection");
+	signDetection ic;
+	ros::spin();
+	return 0;
+}
+
