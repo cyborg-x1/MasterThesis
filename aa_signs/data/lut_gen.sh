fa5e421aa461cdc9fa701a8ba6431b443e7030ce
Christian Holl <cyborgx1@gmail.com>
2012-04-23 14:19:15 +0000

    New package for signDetection

---
+++ aa_signs/data/lut_gen.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+self="${0#./}"
+base="${self%/*}"
+current=$(pwd)
+
+#Get script dir
+if [ "$base" = "$self" ] ; then
+	cd "$current"
+else
+	cd "$current/$base"
+fi ;
+
+echo "Script dir is:" $(pwd)
+	
+#Create LUT
+	
+i=0
+j=0
+out=""
+out2=""
+
+for z in $(cat $(pwd)/Kinectvalues)
+do
+	
+	v=$(echo $z | cut -d"," -f1)
+	
+	#Depth to step LUT
+	for ((; j < v; j++)) ; do
+		out="${out} -1,"
+	done
+	out="${out} $i,"
+	j=$[$j+1]
+
+	
+	#Step to Depth LUT
+	out2="${out2} $v, "
+	
+	
+	i=$[$i+1]
+done
+
+#Ending
+out="${out} -2"
+out2="${out2} -2"
+
+
+#Write header
+echo -e "const short DiscreteFillAndSmoothFilter::kinect_depth_to_step_LUT[] = { $out };" >> ../include/kinectStepLUT
+
+echo -e "const short DiscreteFillAndSmoothFilter::kinect_depths_count = $j;" >> ../include/kinectStepLUT
+	
+echo -e "const short DiscreteFillAndSmoothFilter::kinect_step_to_depth_LUT[] = { $out2 };" >> ../include/kinectStepLUT
+
+echo -e "const short DiscreteFillAndSmoothFilter::kinect_steps_count = $i;" >> ../include/kinectStepLUT
+
+
+indent ../include/kinectStepLUT 
+rm ../include/kinectStepLUT~
